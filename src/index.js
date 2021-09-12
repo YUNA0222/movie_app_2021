40b5bd5d1965577f6e4dec86067c50c916db67ba
YUNA0222 <gold8077@naver.com>
2021-09-12 22:49:28 +0900

    Edit App.js and index.js
    DOM 연습

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+ReactDOM.render(<App />, document.getElementById('root')//id값이 root인 태그에 <App>(App의 리턴값)을 받아서 index.html의 id가 root인 div 사이에 들어가게 됨 50p
 );
