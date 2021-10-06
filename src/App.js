84b4e59a57b3aeb9e25ca2aa49f64c5901e3e9b1
YUNA0222 <gold8077@naver.com>
2021-10-06 11:43:17 +0900

    Edit App.js
    import axios

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
-import { render } from "@testing-library/react";
+import React from "react";
+import axios from "axios";
 
-const foodILike = [
-  {
-    name: 'chikin',
-    image: 'https://img.insight.co.kr/static/2020/01/06/700/u193cc75jr3p97lg645z.jpg'
-  },
-  {
-    name: 'gukbap',
-    image: 'https://cdn.mindgil.com/news/photo/202008/69679_4073_1653.jpg'
+class App extends React.Component{
+  state = {
+    isLoading: true
   }
-]
 
-function renderFood(foo){
-  return <Food name={foo.name} picture={foo.image} />
+  componentDidMount(){
+    setTimeout(()=> {
+      this.setState({isLoading: false})
+      },5000)
+  }
+
+  render() {
+    const{ isLoading }=this.state
+    return( 
+      <div>
+        { isLoading ? 'Loading...' : 'We are ready'}
+      </div>
+    )
+  }
 }
 
-//import Potato from "./Potato";//상대경로
-function App() {//메인 컴포넌트
-  return (
-    <div>
-      {
-        foodILike.map(renderFood)
-      }
-    </div>
-  );
-}
-
-function Food({name, picture}){//서브 컴포넌트
-  // {fav} = props
-  return (
-    <div>
-      <h2>I love {name}</h2>
-      <img src = {picture}/>
-    </div>
-  );
-}
-
-export default App;//외부에서 사용 가능하게 해줌
+export default App;
