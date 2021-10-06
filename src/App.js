b816bef91a82ba3958ee534f14d57921296d5fa1
YUNA0222 <gold8077@naver.com>
2021-10-06 20:05:08 +0900

    Edit App.js
    04-3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import React from "react";
-import axios from "axios";
 
-class App extends React.Component{
-  state = {
-    isLoading: true
-  }
+import propTypes from 'prop-types';
 
-  componentDidMount(){
-    setTimeout(()=> {
-      this.setState({isLoading: false})
-      },5000)
+const foodILike = [
+  {
+    id: 1,
+    name: 'chikin',
+    image: 'https://img.insight.co.kr/static/2020/01/06/700/u193cc75jr3p97lg645z.jpg',
+    rating: 4.9
+  },
+  {
+    id: 2,
+    name: 'gukbap',
+    image: 'https://cdn.mindgil.com/news/photo/202008/69679_4073_1653.jpg',
+    rating: 5.0
   }
+]
 
-  render() {
-    const{ isLoading }=this.state
-    return( 
-      <div>
-        { isLoading ? 'Loading...' : 'We are ready'}
-      </div>
-    )
-  }
+Food.propTypes = {
+  name: propTypes.string.isRequired,
+  picture: propTypes.string.isRequired,
+  rating: propTypes.number.isRequired,
+};
+
+//import Potato from "./Potato";//상대경로z
+function App() {//메인 컴포넌트
+  return (
+    <div>
+      
+        {foodILike.map(
+          dish =>(<Food key={dish.id} name = {dish.name} picture = {dish.image} rating={dish.rating} />)
+        )};
+      
+    </div>
+  );
 }
 
-export default App;
+function Food({name, picture, rating}){//서브 컴포넌트
+  // {fav} = props
+  return (
+    <div>
+      <h2>I love {name}</h2>
+      <h4>{rating}/5.0 </h4>
+      <img src = {picture} alt={name}/>
+    </div>
+  );
+}
+
+export default App;//외부에서 사용 가능하게 해줌
